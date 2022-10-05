c00f35f66e2acf8fb56d10a7931e87f98dc72adc
Daniel Kinyo <kinyodaniel@gmail.com>
2022-10-05 03:30:48 +0300

    update landing page

--- pages/index.vue
+++ pages/index.vue
@@ -85,7 +85,12 @@
                     <div class="left-text">
                         <p>This template is <a href="#">last updated on 20 August 2019 </a>for main menu drop-down arrow and sub menu text color. Duis auctor dolor eu scelerisque vestibulum. Vestibulum lacinia, nisl sit amet tristique condimentum. <br><br>
                         Sed a consequat velit. Morbi lectus sapien, vestibulum et sapien sit amet, ultrices malesuada odio. Donec non quam euismod, mattis dui a, ultrices nisi.</p>
-                        <a href="#about2" class="main-button">Discover More</a>
+                        <a href="#about2" class="">
+                          <img src="https://res.cloudinary.com/selae-learning/image/upload/c_thumb,g_face,r_15,w_200/v1664905155/wankimani/Indigo_White_Modern_Minimal_Typographic_Business_Brand_Identity_Logo_2_xnofnz.png"> </a>
+                          &nbsp;&nbsp;
+                        <a href="#aboutsoma" class="">
+                          <img src="https://res.cloudinary.com/selae-learning/image/upload/c_thumb,g_face,h_39,r_14,w_200/v1664904564/wankimani/Indigo_White_Modern_Minimal_Typographic_Business_Brand_Identity_Logo_3_ew2c2h.png"> </a>
+
                     </div>
                 </div>
             </div>
@@ -105,7 +110,8 @@
             <div class="row">
                 <div class="left-text col-lg-5 col-md-12 col-sm-12 mobile-bottom-fix">
                     <div class="left-heading">
-                        <h5>Curabitur aliquam eget tellus id porta</h5>
+                        <h5> <a href="#about2" class="">
+                          <img src="https://res.cloudinary.com/selae-learning/image/upload/c_thumb,g_face,r_15,w_200/v1664905155/wankimani/Indigo_White_Modern_Minimal_Typographic_Business_Brand_Identity_Logo_2_xnofnz.png"> </a></br>Curabitur aliquam eget tellus id porta</h5>
                     </div>
                     <p>Proin justo sapien, posuere suscipit tortor in, fermentum mattis elit. Aenean in feugiat purus.</p>
                     <ul>
@@ -140,6 +146,47 @@
     </section>
     <!-- ***** Features Big Item End ***** -->
 
+    <!-- ***** Features Big Item Start ***** -->
+    <section class="section" id="aboutsoma">
+        <div class="container">
+            <div class="row">
+                <div class="left-text col-lg-5 col-md-12 col-sm-12 mobile-bottom-fix">
+                    <div class="left-heading">
+                        <h5> <a href="#about2" class="">
+                          <img src="https://res.cloudinary.com/selae-learning/image/upload/c_thumb,g_face,h_39,r_14,w_200/v1664904564/wankimani/Indigo_White_Modern_Minimal_Typographic_Business_Brand_Identity_Logo_3_ew2c2h.png"> </a></br>Curabitur aliquam eget tellus id porta</h5>
+                    </div>
+                    <p>Proin justo sapien, posuere suscipit tortor in, fermentum mattis elit. Aenean in feugiat purus.</p>
+                    <ul>
+                        <li>
+                            <img src="./../assets/images/about-icon-01.png" alt="">
+                            <div class="text">
+                                <h6>Nulla ultricies risus quis risus</h6>
+                                <p>Proin justo sapien, posuere suscipit tortor in, fermentum mattis elit. Aenean in feugiat purus.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="./../assets/images/about-icon-02.png" alt="">
+                            <div class="text">
+                                <h6>Donec consequat commodo purus</h6>
+                                <p>Proin justo sapien, posuere suscipit tortor in, fermentum mattis elit. Aenean in feugiat purus.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="./../assets/images/about-icon-03.png" alt="">
+                            <div class="text">
+                                <h6>Sed placerat sollicitudin mauris</h6>
+                                <p>Proin justo sapien, posuere suscipit tortor in, <a rel="nofollow" href="#">contact</a> Proin justo sapien.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="right-image col-lg-7 col-md-12 col-sm-12 mobile-bottom-fix-big" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
+                    <img src="./../assets/images/right-image.png" class="rounded img-fluid d-block mx-auto" alt="App">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Features Big Item End ***** -->
 
     <!-- ***** Features Small Start ***** -->
     <section class="section" id="services">
